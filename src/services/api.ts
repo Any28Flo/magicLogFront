22516a820bd6cb9670e61ba890c9ff364fb493ef
Analin Flores <foglzerika@gmail.com>
2023-08-19 10:56:39 -0600

    hotfix | update endpoint api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api'; // Replace with your API base URL
+const API_BASE_URL = 'https://magiclogback-production.up.railway.app/api'; // Replace with your API base URL
 
 const api = axios.create({
 	baseURL: API_BASE_URL,
